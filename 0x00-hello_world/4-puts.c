ebf3a5fbebe71e3a3c229c4b4297169d44413211
Marwane-KM <khamsimarouane@gmail.com>
2022-09-11 12:10:47 -0700

    fifth task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+* main - Print Programming is like building a multilingual puzzle
+*
+* Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual
+
+puzzle");
+return (0);
+
+}
