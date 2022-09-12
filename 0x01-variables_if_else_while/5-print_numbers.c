8a63c1e8d15f233ac378e91fad8e40a259d94827
Marwane-KM <111206853+Marwane-KM@users.noreply.github.com>
2022-09-12 23:27:21 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,10 @@
 
 
 /**
-
- * main - Prints all single digit numbers of base 10 starting from 0.
-
- *
-
- * Return: Always 0.
-
- */
+* main - Prints all single digit numbers of base 10 starting from 0.
+*
+*Return: Always 0.
+*/
 
 int main(void)
 
