32b79d6711bbd5173a73585200f16973a722bb47
Marwane-KM <111206853+Marwane-KM@users.noreply.github.com>
2022-09-12 23:36:34 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,14 +3,10 @@
 
 
 /**
-
- * main - Prints all the numbers of base 16 in lowercase.
-
- *
-
- * Return: Always 0.
-
- */
+* main - Prints all the numbers of base 16 in lowercase
+*
+*Return: 0
+*/
 
 int main(void)
 
